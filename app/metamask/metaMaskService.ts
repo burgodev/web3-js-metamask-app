69e22f6b6577fecd30ce6e1e6ddb77fcabc49e07
filipe.burgonovo <burgodev@gmail.com>
2025-01-06 22:14:58 -0300

    feat: get wallet balance

--- app/metamask/metaMaskService.ts
+++ app/metamask/metaMaskService.ts
@@ -1,6 +1,13 @@
 import Web3 from "web3";
 
-const getMetaMaskProvider = async () => {
+declare global {
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ethereum: any;
+  }
+}
+
+export const getMetaMaskProvider = async () => {
   if (!window.ethereum) {
     throw new Error("MetaMask not found!");
   }
@@ -22,4 +29,8 @@ const getMetaMaskProvider = async () => {
   return web3;
 };
 
-export default getMetaMaskProvider;
+export const getBalance = async (address: string) => {
+  const web3 = await getMetaMaskProvider();
+  const balance = await web3.eth.getBalance(address);
+  return balance;
+};
