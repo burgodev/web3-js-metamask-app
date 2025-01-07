2b3028722168171b9e4addcc7fb5ab186fbaf354
filipe.burgonovo <burgodev@gmail.com>
2025-01-06 22:17:27 -0300

    getBalance: display balance

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
-import getMetaMaskProvider from "./metamask/metaMaskService";
+"use client";
+
+import { useState } from "react";
+import { getMetaMaskProvider, getBalance } from "./metamask/metaMaskService";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+
+  const handleGetBalance = async () => {
+    const balance = await getBalance();
+    setMessage(balance.toString());
+  };
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Hello world</h1>
-        <button onClick={() => getMetaMaskProvider()}>Get MetaMask</button>
+        <button onClick={() => getMetaMaskProvider()} className="rounded">
+          Get MetaMask
+        </button>
+        <button onClick={() => handleGetBalance()} className="rounded">
+          Get Balance
+        </button>
+        {message}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p>Powered by @burgoDev</p>
