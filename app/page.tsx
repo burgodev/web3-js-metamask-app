cfffaf5deb178ba6eff6e2c0b1f8af330f9fbe91
filipe.burgonovo <burgodev@gmail.com>
2025-01-07 23:31:16 -0300

    feat: add header

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,10 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col justify-between items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <h1>Metamask Wallet</h1>
+      </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <button onClick={() => getMetaMaskProvider()} className="rounded">
           Get MetaMask
