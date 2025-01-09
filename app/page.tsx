a26bdf6bed1de2e4e27bde0ede84553400efd623
filipe.burgonovo <burgodev@gmail.com>
2025-01-08 21:46:29 -0300

    feat: only display login metamask button if its not logged yet

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import {
 
 export default function Home() {
   const [message, setMessage] = useState("");
+  const [address, setAddress] = useState<string | undefined>(undefined);
 
   const handleGetBalance = async () => {
     const balance = await getBalance();
@@ -23,31 +24,48 @@ export default function Home() {
     setMessage(transferMessage[0].toString());
   };
 
+  const handleLogin = async () => {
+    const web3 = await getMetaMaskProvider();
+
+    const address = await web3.eth.requestAccounts();
+
+    setAddress(address[0]);
+  };
+
   return (
     <div className="flex flex-col justify-between items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-orbitron bg-gradient-to-b from-gray-900 to-black text-white">
       <header className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <h1 className="text-[48px]">Metamask Wallet</h1>
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <button
-          onClick={() => getMetaMaskProvider()}
-          className="rounded-lg bg-gradient-to-r from-blue-900 to-blue-500 text-white px-6 py-3 hover:from-blue-800 hover:to-blue-400 transition-all duration-300 shadow-lg hover:shadow-xl"
-        >
-          Get MetaMask
-        </button>
-        <button
-          onClick={() => handleGetBalance()}
-          className="rounded-lg bg-gradient-to-r from-blue-900 to-blue-500 text-white px-6 py-3 hover:from-blue-800 hover:to-blue-400 transition-all duration-300 shadow-lg hover:shadow-xl"
-        >
-          Get Balance
-        </button>
-        <button
-          onClick={() => handleTransfer()}
-          className="rounded-lg bg-gradient-to-r from-blue-900 to-blue-500 text-white px-6 py-3 hover:from-blue-800 hover:to-blue-400 transition-all duration-300 shadow-lg hover:shadow-xl"
-        >
-          Transfer
-        </button>
-        {message}
+      <main className="flex gap-8 row-start-2 items-center sm:items-start">
+        {!address && (
+          <button
+            onClick={handleLogin}
+            className="rounded-lg bg-gradient-to-r from-blue-900 to-blue-500 text-white px-6 py-3 hover:from-blue-800 hover:to-blue-400 transition-all duration-300 shadow-lg hover:shadow-xl"
+          >
+            Login on MetaMask
+          </button>
+        )}
+        {address && (
+          <div className="flex gap-4 items-center">
+            <div>
+              <button
+                onClick={() => handleGetBalance()}
+                className="rounded-lg bg-gradient-to-r from-blue-900 to-blue-500 text-white px-6 py-3 hover:from-blue-800 hover:to-blue-400 transition-all duration-300 shadow-lg hover:shadow-xl"
+              >
+                Get Balance
+              </button>
+
+              {message}
+            </div>
+            <button
+              onClick={() => handleTransfer()}
+              className="rounded-lg bg-gradient-to-r from-blue-900 to-blue-500 text-white px-6 py-3 hover:from-blue-800 hover:to-blue-400 transition-all duration-300 shadow-lg hover:shadow-xl"
+            >
+              Transfer
+            </button>
+          </div>
+        )}
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p>Powered by @burgoDev</p>
